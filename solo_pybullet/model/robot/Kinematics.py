Fix MGD, MGI, Jacobian because of the new robot modeling (near to the URDF modeling)

solo_pybullet/model/robot/Kinematics.py
@@ -28,20 +28,30 @@ class Kinematics:
                                     [0, 0, 1, 0],
                                     [0, 0, 0, 1]])}
 
-        # self.L[0] = 0
-        # self.L[1] = 0
+    def forward_kinematics(self, Q, dQ):
+        P = np.zeros(12, )
+        dP = np.zeros(12, )
 
-    def forward_kinematics(self, Q):
-        list = []
         for i in range(4):
-            list.append(self.__forward_kinematics(Q[3 * i: 3 * (i + 1)], self.r[self.labels[i]]))
-        return list
+            P[3 * i: 3 * (i + 1)] = self.__forward_kinematics(Q[3 * i: 3 * (i + 1)], self.r[self.labels[i]])
+            dP[3 * i: 3 * (i + 1)] = self.__linearJacobian(Q[3 * i: 3 * (i + 1)]) @ dQ[3 * i: 3 * (i + 1)]
 
-    def inverse_kinematics(self, pos):
+        return P, dP
+
+    def inverse_kinematics(self, P, dP):
         Q = np.zeros((12,))
-        for i in range(len(pos)):
-            Q[3 * i: 3 * (i + 1)] = self.__inverse_kinematics(pos[i], self.r[self.labels[i]])
-        return Q
+        dQ = np.zeros((12,))
+
+        for i in range(4):
+            Q[3 * i: 3 * (i + 1)] = self.__inverse_kinematics(P[3 * i: 3 * (i + 1)])
+            dQ[3 * i: 3 * (i + 1)] = np.linalg.pinv(self.__linearJacobian(Q[3 * i: 3 * (i + 1)])) @ dP[3 * i: 3 * (i + 1)]
+        return Q, dQ
+
+    def linearJacobian(self, Q):
+        J = np.zeros((3, 12))
+        for i in range(4):
+            J[:, 3 * i: 3 * (i + 1)] = self.__linearJacobian(Q[3 * i: 3 * (i + 1)])
+        return J
 
     def __forward_kinematics(self, Q, R):
         q1, q2, q3 = Q
@@ -49,50 +59,60 @@ class Kinematics:
         T04 = np.matrix([[c(q1) * c(q2) * c(q3) - c(q1) * s(q2) * s(q3),
                            -c(q1) * c(q2) * s(q3) - c(q1) * c(q3) * s(q2), -s(q1),
                            -L7 * (c(q1) * c(q2) * c(q3) - c(q1) * s(q2) * s(q3)) - L6 * s(q1) + c(q1) * c(
-                               q2) * L5 - L4 * s(q1) - L2],
+                               q2) * L5 - (L3 + L4) * s(q1) - L2],
                           [c(q3) * s(q2) + c(q2) * s(q3), -s(q2) * s(q3) + c(q2) * c(q3), 0,
-                           -L7 * (c(q3) * s(q2) + c(q2) * s(q3)) + s(q2) * L5 - (L1 + L3)],
+                           -L7 * (c(q3) * s(q2) + c(q2) * s(q3)) + s(q2) * L5 - L1],
                           [s(q1) * c(q2) * c(q3) - s(q1) * s(q2) * s(q3),
                            -s(q1) * c(q2) * s(q3) - s(q1) * c(q3) * s(q2), c(q1),
                            -L7 * (s(q1) * c(q2) * c(q3) - s(q1) * s(q2) * s(q3)) + L6 * c(q1) + s(q1) * c(
-                               q2) * L5 + L4 * c(q1)],
+                               q2) * L5 + (L3 + L4) * c(q1)],
                           [0, 0, 0, 1]])
 
         return R * T04
 
-    def __inverse_kinematics(self, pos, R):
+    def __inverse_kinematics(self, pos):
         L1, L2, L3, L4, L5, L6, L7 = self.L
 
         # compute q1
         X = pos[2]
         Y = -pos[0] - L2
-        Z = L4 + L6
+        Z = L3 + L4 + L6
         q1 = np.arctan2((Y * Z + X * np.sqrt(X * X + Y * Y - Z * Z)) / (X * X + Y * Y),
                         (X * Z - Y * np.sqrt(X * X + Y * Y - Z * Z)) / (X * X + Y * Y))
 
         # compute q3
-        Z1 = (pos[2] - np.cos(q1) * (L4 + L6)) / np.sin(q1)
-        Z2 = pos[1] + L1 + L3
+        Z1 = (pos[2] - np.cos(q1) * (L3 + L4 + L6)) / np.sin(q1)
+        Z2 = pos[1] + L1
         c3 = (Z1 * Z1 + Z2 * Z2 - L5 * L5 - L7 * L7) / (-2 * L5 * L7)
         q3_1 = np.arctan2(np.sqrt(1 - c3 * c3), c3)
-        q3_2 = np.arctan2(-np.sqrt(1 - c3 * c3), c3)
+        # q3_2 = np.arctan2(-np.sqrt(1 - c3 * c3), c3)
 
         # compute q2
         B1_1 = L5 - L7 * np.cos(q3_1)
-        B1_2 = L5 - L7 * np.cos(q3_2)
+        # B1_2 = L5 - L7 * np.cos(q3_2)
         B2_1 = -L7 * np.sin(q3_1)
-        B2_2 = -L7 * np.sin(q3_2)
+        # B2_2 = -L7 * np.sin(q3_2)
         q2_1 = np.arctan2((B1_1 * Z2 - B2_1 * Z1) / (B1_1 * B1_1 + B2_1 * B2_1),
                           (B1_1 * Z1 + B2_1 * Z2) / (B1_1 * B1_1 + B2_1 * B2_1))
-        q2_2 = np.arctan2((B1_2 * Z2 - B2_2 * Z1) / (B1_2 * B1_2 + B2_2 * B2_2),
-                          (B1_2 * Z1 + B2_2 * Z2) / (B1_2 * B1_2 + B2_2 * B2_2))
+        # q2_2 = np.arctan2((B1_2 * Z2 - B2_2 * Z1) / (B1_2 * B1_2 + B2_2 * B2_2),
+        #                   (B1_2 * Z1 + B2_2 * Z2) / (B1_2 * B1_2 + B2_2 * B2_2))
 
         return np.array([q1, q2_1, q3_1])
+
+    def __linearJacobian(self, Q):
+        q1, q2, q3 = Q
+        L1, L2, L3, L4, L5, L6, L7 = self.L
+
+        return np.matrix([[c(q1) * (-L3 - L4 - L6) - s(q1) * (c(q2) * L5 - L7 * c(q2 + q3)),
+                           c(1) * (L7 * s(q2 + q3) - s(q2) * L5), c(q1) * L7 * s(q2 + q3)],
+                          [0, c(q2) * L5 - L7 * c(q2 + q3), -L7 * c(q2 + q3)],
+                          [s(q1) * (-L3 - L4 - L6) + c(q1) * (c(q2) * L5 - L7 * c(q2 + q3)),
+                           s(1) * (L7 * s(q2 + q3) - s(q2) * L5), s(q1) * L7 * s(q2 + q3)]])
 
     def __T01(self, q1):
         L = self.L
         return np.matrix([[c(q1), -s(q1), 0, -L[1]],
-                          [0, 0, -1, -(L[0] + L[2])],
+                          [0, 0, -1, -L[0]],
                           [s(q1), c(q1), 0, 0],
                           [0, 0, 0, 1]])
 
@@ -106,7 +126,7 @@ class Kinematics:
         L = self.L
         return np.matrix([[c(q3), -s(q3), 0, L[4]],
                           [s(q3), c(q3), 0, 0],
-                          [0, 0, 1, L[3]],
+                          [0, 0, 1, L[2] + L[3]],
                           [0, 0, 0, 1]])
 
     def __T34(self):
@@ -133,11 +153,11 @@ def test_1():
     from solo_pybullet.model.robot.Viewer import Viewer
 
     # variables
-    L = [0.15, 0.1, 0.15, 0.03, 0.15, 0.03, 0.15]
+    L = [0.1946, 0.0875, 0.014, 0.03745, 0.16, 0.008, 0.16]
     T = 1
     Lp = 0.15
-    x0 = -(L[1] + L[3] + L[5])
-    y0 = -(L[0] + L[2] - Lp / 2)
+    x0 = -(L[1] + L[2] + L[3] + L[5])
+    y0 = -(L[0] + Lp / 2)
     z0 = -0.2
     H = 0.05
     kinematics = Kinematics(L)
@@ -152,8 +172,8 @@ def test_1():
     for i, t0 in enumerate(t):
         res[0:3, i] = foot_trajectory(t0, T, x0, y0, z0, H, Lp)
         res[3:6, i] = foot_trajectory(t0, T, x0, y0, z0, H, Lp)
-        res[6:9, i] = foot_trajectory(t0, T, x0, y0, z0, H, Lp)
-        res[9:12, i] = foot_trajectory(t0, T, x0, y0, z0, H, Lp)
+        res[6:9, i] = foot_trajectory(t0, T, x0, y0, z0, H, Lp, y=1)
+        res[9:12, i] = foot_trajectory(t0, T, x0, y0, z0, H, Lp, y=1)
 
     Viewer.viewInverseKinematics(kinematics, res)
 

solo_pybullet/model/robot/Viewer.py
@@ -8,6 +8,10 @@ class Viewer:
         pass
 
     @staticmethod
+    def viewConfigurations(kinematics, jacobian):
+        pass
+
+    @staticmethod
     def viewForwardKinematics(kinematics):
         f, ax = plt.subplots()
         f.suptitle(r'Visualisation du MGD')
@@ -43,9 +47,6 @@ class Viewer:
             ax.clear()
             draw()
 
-            t = kinematics.T0k(q, 4, kinematics.r[kinematics.labels[1]])[:-1, 3].reshape(-1)
-            kinematics.inverse_kinematics([np.asarray(t).reshape(-1)])[3:6]
-
         # sliders
         for i in range(3):
             ax_q[i] = f.add_axes([0.05 + 0.05 * i, 0.1, 0.0225, 0.8])
@@ -66,7 +67,7 @@ class Viewer:
         pos = np.zeros((5 * points.shape[1], 3, 4))
 
         for i in range(points.shape[1]):
-            Q[:, i] = kinematics.inverse_kinematics([points[3 * j: 3 * (j + 1), i] for j in range(4)])
+            Q[:, i], _ = kinematics.inverse_kinematics(points[:, i], np.zeros((12,)))
             for j in range(1, 5):
                 for k in range(4):
                     pos[5 * i + j, :, k] = np.transpose(kinematics.T0k(Q[3 * k:3 * (k + 1), i], j, kinematics.r[kinematics.labels[k]])[:-1, 3])